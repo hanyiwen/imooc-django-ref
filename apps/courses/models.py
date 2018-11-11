9831c03325ebbce55b7fe1fb25229ba208707c01
hanyiwen <hanyiwen@tju.edu.cn>
2018-11-11 23:55:49 +0800

    update

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -34,6 +34,7 @@ class Course(models.Model):
     def get_zj_nums(self):
         return self.lesson_set.all().count()
 
+    # 谁的里面添加了它做外键，他都可以取出来
     def get_learn_users(self):
         return self.usercourse_set.all()[:5]
 
